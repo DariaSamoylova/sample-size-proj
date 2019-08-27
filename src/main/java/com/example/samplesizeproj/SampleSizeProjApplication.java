9da6cc6b0a288a6ba457cb37532307e5787a0bf0
DariaSamoylova <daria.borodina@gmail.com>
2019-08-27 12:23:44 +0300

    spring boot initlzr

---
+++ src/main/java/com/example/samplesizeproj/SampleSizeProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplesizeproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSizeProjApplication {
+
+    public  static void main(String[] args) {
+        SpringApplication.run(SampleSizeProjApplication.class, args);
+    }
+
+}
